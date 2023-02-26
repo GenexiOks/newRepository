c70f0b9e0028c76b6edb04431d1ac1d5447d5a44
Your Name <you@example.com>
2023-02-26 06:52:07 +0300

    new up

---
+++ script.js
@@ -0,0 +1,18 @@
+// const hamburger = 5;
+// const fries = 0;
+
+// if (hamburger && fries) {
+//     console.log('I am fine');
+// }
+
+const hamburger = 3;
+const fries = 1;
+const cola = 2;
+
+console.log((hamburger === 3 && cola && fries));
+
+if (hamburger === 3 && cola && fries) {
+    console.log('All fine');
+} else {
+    console.log('Exit')
+}
